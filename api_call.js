aa63dbcced24e112e804b97deea088ba485f01a5
jmc617 <jmc617@gmail.com>
2019-04-12 19:59:03 -0400

    api call url changes

--- api_call.js
+++ api_call.js
@@ -1,16 +1,26 @@
+function apiCall() {
+
+//prevents CORS error
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
+//change url to get appropriate smaller word list
 const url = "http://app.linkedin-reach.io/words"; 
 
-
+//move word array into other file where imported
+let wordArray = []
 
 fetch(proxyurl + url)
 .then(function(response) {
     return response.text()
 })
 .then(text => {
+    //remove logs
     console.log(text); 
     const wordList = text.split('\n')
-    console.log(wordList)
+    wordArray = wordList.slice()
+    console.log(wordArray)
 })
 .catch(error => console.log(error))
+
+}
+
 
